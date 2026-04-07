f5ad8098c7206bfde3f77dcd34ac704af1dac86e
ejiseok <jiseoklee0502@gmail.com>
2026-04-07 20:36:55 +0900

    HTML, CSS webpack 설정

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 import path from "node:path";
+import HtmlWebpackPlugin from "html-webpack-plugin";
 
 export default {
   mode: "development",
@@ -7,5 +8,18 @@ export default {
     filename: "main.js",
     path: path.resolve(import.meta.dirname, "dist"),
     clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/template.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
   },
 };
