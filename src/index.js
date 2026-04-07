90a644210009154ad6a3500a7d50e86ee27a9f79
ejiseok <jiseoklee0502@gmail.com>
2026-04-07 20:53:53 +0900

    버튼 이벤트 추가

--- src/index.js
+++ src/index.js
@@ -4,8 +4,25 @@ import { homeContent } from "./homeContent.js";
 import { menuContent } from "./menuContent.js";
 import { aboutContent } from "./aboutContent.js";
 
+const homeBtn = document.querySelector(".home-btn");
+const menuBtn = document.querySelector(".menu-btn");
+const aboutBtn = document.querySelector(".about-btn");
+
 const content = document.querySelector("#content");
 
+homeBtn.addEventListener("click", () => {
+  content.textContent = "";
+  content.append(homeContent);
+});
+
+menuBtn.addEventListener("click", () => {
+  content.textContent = "";
+  content.append(menuContent);
+});
+
+aboutBtn.addEventListener("click", () => {
+  content.textContent = "";
+  content.append(aboutContent);
+});
+
 content.append(homeContent);
-content.append(menuContent);
-content.append(aboutContent);
