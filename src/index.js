322aaad58afd66a67d9731daed1f22b7ff622d22
ejiseok <jiseoklee0502@gmail.com>
2026-04-07 20:46:27 +0900

    본문 컨텐츠 자바스크립트를 통해 생성하는 것으로 변경

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 import "./styles.css";
 
-console.log("this is index.js");
+const content = document.querySelector("#content");
+
+const homeContent = document.createElement("h2");
+homeContent.textContent = "멋진 레스토랑!";
+
+const menuContent = document.createElement("h2");
+menuContent.textContent = "맛있는 음식!";
+
+const aboutContent = document.createElement("h2");
+aboutContent.textContent = "예쁜 연락처!";
+
+content.append(homeContent);
+content.append(menuContent);
+content.append(aboutContent);
